8dbfcf87042c1e3cb5065c9bf3e0a9d6562673f4
An Pham <anpham.0992@gmail.com>
2025-07-02 12:27:16 -0400

    done with submit issue, view issues pages, create project on firebase

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAE-DtcOhs8PBwY6-sWkXO0AA66-L5ODok",
+  authDomain: "voicesofresidents.firebaseapp.com",
+  projectId: "voicesofresidents",
+  storageBucket: "voicesofresidents.firebasestorage.app",
+  messagingSenderId: "635648116227",
+  appId: "1:635648116227:web:642848b4cfabc4534d7036"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
